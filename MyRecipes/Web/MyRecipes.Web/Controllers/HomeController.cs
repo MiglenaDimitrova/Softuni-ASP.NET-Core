e19f244f80eb324fb3bae358cafdbbc97edf3290
Miglena Dimitrova <mimas4emn@gmail.com>
2021-07-14 08:07:06 +0300

    Add RecipesAndRelatedModels

--- MyRecipes/Web/MyRecipes.Web/Controllers/HomeController.cs
+++ MyRecipes/Web/MyRecipes.Web/Controllers/HomeController.cs
@@ -2,9 +2,8 @@
 {
     using System.Diagnostics;
 
-    using MyRecipes.Web.ViewModels;
-
     using Microsoft.AspNetCore.Mvc;
+    using MyRecipes.Web.ViewModels;
 
     public class HomeController : BaseController
     {
